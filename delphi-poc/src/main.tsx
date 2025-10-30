a76557796751233183a25088f2ee494d14bd8ec4
unknown <jiteshvarade1@gmail.com>
2025-10-31 00:26:35 +0530

    Setup app routing

--- delphi-poc/src/main.tsx
+++ delphi-poc/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ArticlePage from './pages/ArticlePage'
+import LogsPage from './pages/LogsPage'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/article/:id" element={<ArticlePage />} />
+        <Route path="/logs" element={<LogsPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
