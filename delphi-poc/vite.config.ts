acf21e5eb72e0b6a70a96a8b28c45f340178f1f1
unknown <jiteshvarade1@gmail.com>
2025-10-30 23:43:54 +0530

    Installed dependencies and configure vitest

--- delphi-poc/vite.config.ts
+++ delphi-poc/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'   // <-- change this line
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
 })
